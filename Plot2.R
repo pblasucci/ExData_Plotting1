61ad73a34bfac40800b8376d5d13a27cdc06a06c
Paulmichael Blasucci <pblasucci@gmail.com>
2016-07-08 16:56:53 -0400

    fixed goof in Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -8,10 +8,10 @@ EPC <- read.table('household_power_consumption_2007_FEB_01_02.txt'
 EPC$Time <- as.POSIXlt(paste(EPC$Date,EPC$Time),format = "%d/%m/%Y %H:%M:%S")
 EPC$Date <- as.Date(EPC$Date, format = "%d/%m/%Y")
 
-#png(filename = "Plot2.png")
+png(filename = "Plot2.png")
 plot(EPC$Time, EPC$Global_active_power
      ,type = 's'
      ,main = ""
      ,xlab = ""
      ,ylab = "Global Active Power (kilowatts)")
-#dev.off()
+dev.off()
